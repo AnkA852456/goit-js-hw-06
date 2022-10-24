841dd68a3893ace0deb009ffe57424d597bc41ca
AnkA852456 <108530192+AnkA852456@users.noreply.github.com>
2022-10-24 18:06:28 +0300

    1-9 tasks done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+};
 
+refs.form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (
+    event.currentTarget.elements.email.value === "" ||
+    event.currentTarget.elements.password.value === ""
+  ) {
+    alert("All inputs must be filled!");
+  }
+
+  const cred = {
+    Email: event.currentTarget.elements.email.value,
+    Password: event.currentTarget.password.value,
+  };
+  console.log(cred);
+
+  refs.form.reset();
+}
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const refs = {
+  spanEl: document.querySelector(".color"),
+  changeColorBtn: document.querySelector(".change-color"),
+  bodyEl: document.querySelector("body"),
+};
+
+console.log(refs.changeColorBtn);
+
+refs.changeColorBtn.addEventListener("click", onchangeColorBtn);
+
+function onchangeColorBtn(event) {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.spanEl.textContent = getRandomHexColor();
 }
